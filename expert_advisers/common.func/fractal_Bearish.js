488a4f7d465542e469a147bbb6b722dd0c33cbfb
IvanPazhgin <ivan.pazhgin@gmail.com>
2022-09-17 10:06:26 +0300

    Create: поиск фракталов

---
+++ expert_advisers/common.func/fractal_Bearish.js
@@ -0,0 +1,33 @@
+const timestampToDateHuman = require('./timestampToDateHuman')
+
+function fractal_Bearish(array, start, finish) {
+  let fractal_Bearish = {
+    isFractal: false,
+    high: 0,
+    time: 0,
+  }
+  for (let i = start; i <= finish; i++) {
+    // ищем Bearish (медвежий) Fractal. Факртал находится на позиции [i-2]
+    if (
+      array[i - 4].high < array[i - 2].high &&
+      array[i - 3].high < array[i - 2].high &&
+      array[i - 1].high < array[i - 2].high &&
+      array[i].high < array[i - 2].high
+    ) {
+      fractal_Bearish = {
+        nameFracral: 'Bearish',
+        nameFracralRus: 'Меджвежий фрактал',
+        isFractal: true,
+        high: array[i - 2].high,
+        time: array[i - 2].startTime,
+        // проверочная ифнормация
+        timeH: timestampToDateHuman(array[i - 2].startTime),
+        i: finish,
+        iH: timestampToDateHuman(array[i].startTime),
+      }
+      return fractal_Bearish
+    } else return fractal_Bearish
+  }
+}
+
+module.exports = fractal_Bearish
---
+++ expert_advisers/common.func/fractal_Bullish.js
@@ -0,0 +1,33 @@
+const timestampToDateHuman = require('./timestampToDateHuman')
+
+function fractal_Bullish(array, start, finish) {
+  let fractal_Bullish = {
+    isFractal: false,
+    low: 0,
+    time: 0,
+  }
+  for (let i = start; i <= finish; i++) {
+    // ищем Bullish (бычий) Fractal. Факртал находится на позиции [i-2]
+    if (
+      array[i - 4].low > array[i - 2].low &&
+      array[i - 3].low > array[i - 2].low &&
+      array[i - 1].low > array[i - 2].low &&
+      array[i].low > array[i - 2].low
+    ) {
+      fractal_Bullish = {
+        nameFracral: 'Bullish',
+        nameFracralRus: 'Бычий фрактал',
+        isFractal: true,
+        low: array[i - 2].low,
+        time: array[i - 2].startTime,
+        // проверочная ифнормация
+        timeH: timestampToDateHuman(array[i - 2].startTime),
+        i: finish,
+        iH: timestampToDateHuman(array[i].startTime),
+      }
+      return fractal_Bullish
+    } else return fractal_Bullish
+  }
+}
+
+module.exports = fractal_Bullish
